7875d51d98843fc2c045d0fcf8654cfdc52c8287
Ben Marwick <benmarwick@gmail.com>
2014-07-18 11:11:07 +0200

    about to upload to repo

--- code/libraries.r
+++ code/libraries.r
@@ -3,17 +3,6 @@
 
 # libraries need to be in a .R file so packrat can find them
 
-# require(ggplot2)
-# require(reshape2)
-# require(plyr)
-# require(dplyr)
-# require(vcd)
-# require(rjags)
-# require(coda)
-# require(BEST)
-# require(lattice)
-# require(xtable)
-# require(data.table)
 
 
 
@@ -63,4 +52,17 @@ lapply(github_pkgs, ipak_github)
 #   sapply(pkg2, require, character.only = TRUE)
 # }
 # ipak_github_reshape("reshape", "reshape2")
+
+
+require(ggplot2)
+require(reshape2)
+require(plyr)
+require(dplyr)
+require(vcd)
+require(rjags)
+require(coda)
+require(BEST)
+require(lattice)
+require(xtable)
+require(data.table)
 
--- code/packrat.r
+++ code/packrat.r
@@ -8,12 +8,12 @@
 
 # do this periodically to see if I've added any packages that haven't
 # been locally stored. Want to do this each time I work on the project.
-packrat::status()
+# packrat::status()
 
 # if the status says I have some packages that aren't yet in the
 # packrat. Can run every time I work on the project & add or remove a 
 # package
-packrat::snapshot() 
+# packrat::snapshot() 
 
 # if the status says it found packages I'm not using, use this to clean them # out
 # packrat::clean()
